1986f78f4f9052c1798a09e32249e4ef06de54d2
phphcc <89339504+phphcc@users.noreply.github.com>
2021-08-22 17:26:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    introTitle: 'Babe ahhh!',
+    introDesc: `Trái đất đầy rẫy tai nạn
+    Mà ngã vào tim anh là tai nạn tuyệt vời nhất
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng anh đã say  ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười của emm ',
+    btnYes: 'Vẫn cứ là thích bé <33',
+    btnNo: 'Không, bé  à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại chọn yêu bé:<',
+    btnReply: 'Gửi cho tuii <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Bé biết mà😆. Yêu cục cưng nhiều nhiều🥺',
+    messDesc: 'Hẹn anh hết dịch nha công túa của emmm🥺.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100021942156918'
 }
